5a0abe3599a52b63e4170145332554104d211e93
Ali Pourhassan <67645398+aliph55@users.noreply.github.com>
2025-02-16 15:55:31 +0300

    master

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import * as Google from "expo-auth-session/providers/google";
 import { useEffect, useState } from "react";
 import { Button, View, Text } from "react-native";
 import * as WebBrowser from "expo-web-browser";
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 //import { initializeApp } from "firebase/app";
 
@@ -9,16 +14,20 @@ WebBrowser.maybeCompleteAuthSession();
 
 // Firebase Config (Firebase Console'dan al)
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
+  apiKey: "AIzaSyB1Sg1c8LKwp8162aQO9Ws48AqjzXBmpS8",
+  authDomain: "ninja-ddda1.firebaseapp.com",
+  databaseURL: "https://ninja-ddda1.firebaseio.com",
+  projectId: "ninja-ddda1",
+  storageBucket: "ninja-ddda1.firebasestorage.app",
+  messagingSenderId: "491406833717",
+  appId: "1:491406833717:web:d24c9b389eb48149dad97c",
 };
 
-//const app = initializeApp(firebaseConfig);
-//const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
 export default function GoogleSignIn() {
   const [user, setUser] = useState(null);
@@ -30,19 +39,32 @@ export default function GoogleSignIn() {
       "580447110631-vvv3d82c0e3sotaeacnkigi5cu2ks62a.apps.googleusercontent.com",
     iosClientId:
       "580447110631-esckavplmrljkgnf5v299aerufajuvdb.apps.googleusercontent.com",
-    //  androidClientId: "YOUR_ANDROID_CLIENT_ID",
+    androidClientId:
+      "580447110631-pgrpip2f74n1oh2e6ovf48t7asv45c31.apps.googleusercontent.com",
   });
 
   useEffect(() => {
+    const unsubscrible = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+    });
+    return () => {
+      unsubscrible();
+    };
+  }, []);
+
+  const [userInfo, setUserInfo] = useState();
+  useEffect(() => {
     if (response?.type === "success") {
       const { id_token } = response.params;
-      //  const credential = GoogleAuthProvider.credential(id_token);
-      //  signInWithCredential(auth, credential)
-      //    .then((userCredential) => setUser(userCredential.user))
-      //   .catch((error) => console.error(error));
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential)
+        .then((userCredential) => setUserInfo(userCredential.user))
+        .catch((error) => console.error(error));
     }
-    console.log(response.params);
+    console.log(response?.params);
   }, [response]);
+
+  console.log(userInfo);
 
   return (
     <View
@@ -50,6 +72,7 @@ export default function GoogleSignIn() {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
+        backgroundColor: "blue",
       }}
     >
       <Button title="Google ile Giriş Yap" onPress={() => promptAsync()} />
